6fbe4298b393c2e9eb10b2a0e2eee6bd1554c149
mohit-marwal99 <121860823+mohit-marwal99@users.noreply.github.com>
2023-01-03 15:40:41 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,46 @@
+<header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <span>Centurylink</span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php"> About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="service.php">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="price.php">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Enquiry.php">Enquiry</a>
+              </li>
+               <li class="nav-item">
+                <a class="nav-link" href="../project/Customer/Index.php">LogIn</a>
+              </li>
+               
+            </ul>
+            <div class="quote_btn-container">
+              
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>
+                  Call : +01 123455678990
+                </span>
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
